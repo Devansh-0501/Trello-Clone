c4e0e4c864e0c22138a96b1fbbdc5b0340a56044
DEVANSH-0501 <devansh055leo@gmail.com>
2025-04-16 14:40:15 +0530

    second commit

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,21 +1,52 @@
-import React from 'react' 
-import "../styles/board.css"
-import Addlist from './Addlist'
+import React, { useState } from "react";
+import "../styles/board.css";
 
 const Board = () => {
+  const [listValue, setListValue] = useState("");
+  const [list, SetList] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const addList = () => {
+    SetList([...list, listValue]);
+    setListValue("");
+  };
+
   return (
-    <div className='board'>
+    <div className="board">
       <div className="u-board">
-        <h1>
-          My Trello Board
-        </h1>
+        <h1>My Trello Board</h1>
       </div>
       <div className="main-board">
-        <Addlist/>
-      </div>
-      
-    </div>
-  )
-}
+        <div className="add-list">
+          <input
+            type="text"
+            onClick={() => {
+              setShow(!show);
+            }}
+            onChange={(e) => {
+              setListValue(e.target.value);
+            }}
+            placeholder="Add List"
+          />
 
-export default Board
+          {show && (
+            <div className="list-form">
+              <button onClick={addList}>Add List</button>
+            </div>
+          )}
+        </div>
+        <div className="show-list">
+          {list.map((l, i) => {
+            return (
+              <div key={i} className="single-list">
+                {l}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
